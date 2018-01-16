03397b4b13a5fdce81e7ae375a74668211fc35d2
Bondar Dmytro <bondar.dima.12@gmail.com>
2018-01-16 10:50:09 +0200

    added displaying articles by category

--- GNP/GNP/Controllers/Favorites/ArticlesByCategoryViewController.swift
+++ GNP/GNP/Controllers/Favorites/ArticlesByCategoryViewController.swift
@@ -1,52 +1,37 @@
 //
-//  SavedNewsViewController.swift
-//  GlobalNewsPortal
+//  ArticlesByCategoryViewController.swift
+//  GNP
 //
-//  Created by user on 14/12/2017.
-//  Copyright © 2017 chi. All rights reserved.
+//  Created by user on 16/01/2018.
+//  Copyright © 2018 CHI. All rights reserved.
 //
 
 import UIKit
-import CoreData
-import WebKit
 
-class SavedNewsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, NSFetchedResultsControllerDelegate, PageScrollDelegate{
-    @IBOutlet weak var articleTableView: UITableView!
-    @IBOutlet weak var searchBarButtonItem: UIBarButtonItem!
+class ArticlesByCategoryViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, PageScrollDelegate{
+    @IBOutlet weak var articlesTableView: UITableView!
     
-    private var fetchController: NSFetchedResultsController<Article>?
-    private var articles: [Article] = []
+    var categoryID: String!
+    
+    private var articles: [NotSavedArticle] = []
     private var currentArticleIndex: IndexPath!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         self.setupTableView()
-        self.fetchData()
+        self.fetchArticles(categoryNews: self.categoryID, afterFetchHandler: {
+            self.articlesTableView.reloadData()
+        })
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        self.navigationController?.hidesBarsOnSwipe = true
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        self.navigationController?.isNavigationBarHidden = false
-        self.navigationController?.hidesBarsOnSwipe = false
-        
-        super.viewWillDisappear(animated)
-    }
-    
-    //MARK: Setup
     
     func setupTableView() {
-        self.articleTableView.dataSource = self
-        self.articleTableView.delegate = self
+        self.articlesTableView.dataSource = self
+        self.articlesTableView.delegate = self
         
-        self.articleTableView.register(UINib(nibName: "ArticleCell", bundle: nil), forCellReuseIdentifier: "articleCell")
+        self.articlesTableView.register(UINib(nibName: "ArticleCell", bundle: nil), forCellReuseIdentifier: "articleCell")
         
-        self.articleTableView.backgroundColor = .appColor
+        self.articlesTableView.backgroundColor = .appColor
     }
     
     //MARK: UITableViewDataSource
@@ -56,7 +41,7 @@ class SavedNewsViewController: UIViewController, UITableViewDataSource, UITableV
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = self.articleTableView.dequeueReusableCell(withIdentifier: "articleCell") as! ArticleCell
+        let cell = self.articlesTableView.dequeueReusableCell(withIdentifier: "articleCell") as! ArticleCell
         
         var mode: ArticleCellMode!
         if indexPath.row % 2 == 0 {
@@ -81,7 +66,6 @@ class SavedNewsViewController: UIViewController, UITableViewDataSource, UITableV
         
         let storyboard = UIStoryboard(name: "AllNews", bundle: nil)
         let newsDetailVC = storyboard.instantiateViewController(withIdentifier: "newsDetailVC") as! NewsDetailViewController
-        newsDetailVC.mode = .saved
         newsDetailVC.delegate = self
         
         self.navigationController?.pushViewController(newsDetailVC, animated: true)
@@ -102,14 +86,6 @@ class SavedNewsViewController: UIViewController, UITableViewDataSource, UITableV
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 180
-    }
-    
-    //MARK: NSFetchedResultsControllerDelegate
-    
-    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        self.articles = self.fetchController!.fetchedObjects!
-        
-        self.articleTableView.reloadData()
     }
     
     func startArticle<A>() -> A where A : ArticleProtocol {
@@ -136,19 +112,19 @@ class SavedNewsViewController: UIViewController, UITableViewDataSource, UITableV
         return self.articles[self.currentArticleIndex.row] as! A
     }
     
-    //MARK: Event handlers
-    
-    func fetchData() {
-        if self.fetchController == nil {
-            let fetchRequest: NSFetchRequest<Article> = Article.fetchRequest()
-            fetchRequest.sortDescriptors = [NSSortDescriptor(key: "publishedAt", ascending: false)]
-            self.fetchController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
-            self.fetchController?.delegate = self
-        }
-        
-        try! self.fetchController?.performFetch()
-        
-        self.articles = self.fetchController!.fetchedObjects!
+    func fetchArticles(categoryNews: String?, afterFetchHandler: @escaping ()->Void) {
+        RequestManager.fetchArticles(byKeyWord: nil, categoryNews: categoryNews, completionHandler: {[weak self] success, result, error in
+            if success, let json = result as? [String : Any?] {
+                var newArticles: [NotSavedArticle] = []
+                if let articlesJSON = json["articles"] as? [Any?] {
+                    for articleJSON in articlesJSON {
+                        newArticles.append(NotSavedArticle.parseArticle(fromRawObject: articleJSON))
+                    }
+                }
+                
+                self?.articles = newArticles
+                afterFetchHandler()
+            }
+        })
     }
 }
-
